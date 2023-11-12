54e3d161aadbaa436db0858cb69fd85b6d84f11e
MarianaPadelin <marianapadelin@gmail.com>
2023-11-12 10:14:38 -0300

    ultimas correcciones

--- Desafio3/src/app.js
+++ Desafio3/src/app.js
@@ -1,104 +1,47 @@
-import express from "express";
-import { ProductManager, Product } from "../../desafio2/desafio2.js";
+//Desafío entregable N°3
 
-//correr con npm run dev, abrir el explorador con localhost:5000
-// const express = require('express') => esto se usa con type: common js en package.json
+import express from "express";
+import { ProductManager } from "./desafio2.js";
 
 
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
 
-// 1) Instancio la clase
+// Instancio la clase
 const prueboProducto = new ProductManager();
-
-// 2) Agrego productos y pruebo las validaciones. Si hay campos vacios o si se repite el código, no se agregan
-// prueboProducto.addProduct(
-//   new Product(
-//     "Producto prueba1",
-//     "Este es un producto prueba",
-//     2030,
-//     "SinImagen",
-//     "abc143",
-//     "44"
-//   )
-// );
 
 // console.log(prueboProducto.getProducts());
 
 
-//El archivo json se generó correctamente
-
-
-const arrayPrueba = [
-  {
-    id: 1,
-    nombre: "pepe",
-    edad: 40,
-  },
-  {
-    id: 2,
-    nombre: "mario",
-    edad: 57,
-  },
-  {
-    id: 3,
-    nombre: "soledad",
-    edad: 37,
-  },
-  {
-    id: 4,
-    nombre: "pepe",
-    edad: 71,
-  },
-  {
-    id: 5,
-    nombre: "pepe",
-    edad: 45,
-  },
-];
-
 app.get("/", async (req, res) => {
-  const { nombre, limit } = req.query;
-  if (nombre) {
-    const usuarios = await arrayPrueba.filter((user) => user.nombre === nombre);
+  res.send(
+    "<h1>Inicio</h1><a href='/products'><button>Ver los productos</button></a>"
+  );
+});
 
-    if (usuarios && limit) {
-      usuarios.length = limit;
-      return res.json(usuarios);
-    } else if (usuarios) {
-      return res.json(usuarios);
-    } else {
-      //acá no llega, muestra []
-      return res.send("<h1>Usuario no encontrado</h1>");
-    }
+app.get("/products", async (req, res) => {
+  let datosImportados = await prueboProducto.getProducts();
+  const { limit } = req.query;
+
+  if (limit && limit <= datosImportados.length) {
+    datosImportados.length = limit;
+      res.send(datosImportados);
+
   } else {
-    res.send("<h1>Inicio</h1>");
+    //falta refrescar la pagina  
+    // console.log(datosImportados)
+     res.send(datosImportados);
   }
 });
 
-
-//----Este es el problema, cuando quiero cambiar de arrayPrueba a getProducts me aparece un array vacío. No se si estoy en lo correcto al querer llamar a getProducts, o si deberia importar el archivo json (me parece redundante hacer eso), o si lo tendría que pasar a .stringify, o usar algún otro método
-app.get("/importacion", (req, res) =>res.json(prueboProducto.getProducts()));
-
-app.get("/productos", (req, res) => {
-      const { limit } = req.query;
-
-    if(limit){
-        arrayPrueba.length = limit
-        return res.json(arrayPrueba)
-    } else {
-        return res.json(arrayPrueba)
-    }
- 
-});
-
-app.get("/productos/:id", (req, res) => {
+app.get("/products/:id", async (req, res) => {
+  let datosImportados = await prueboProducto.getProducts();
   const { id } = req.params;
 
-  const usuario = arrayPrueba.find((user) => user.id === +id);
+  const usuario = datosImportados.find((user) => user.id === +id);
 
   if (usuario) {
-    return res.json(usuario);
+    return res.send(usuario);
   } else {
     return res.send(
       `<h1>No existe el id</h1><a href='./'><button>Ver todos los productos</button></a>`
@@ -107,5 +50,5 @@ app.get("/productos/:id", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${5000}`);
+  console.log(`Server listening on port ${PORT}`);
 });
---
+++ Desafio3/src/desafio2.js
@@ -0,0 +1,286 @@
+
+import fs from "fs"
+
+class ProductManager {
+  constructor() {
+    this.path = "./src/datos.json";
+    try {
+      let products = fs.readFileSync(this.path, "utf-8");
+      this.products = JSON.parse(products);
+    } catch {
+      this.products = [];
+    }
+  }
+
+  async addProduct(product) {
+    try {
+      let existeCodigo = false;
+      this.products.forEach((prod) => {
+        prod.code.includes(product.code)
+          ? (existeCodigo = true)
+          : (existeCodigo = false);
+      });
+
+      let datosCompletos = Object.values(product);
+
+      if (existeCodigo === true) {
+        console.log("El código ya existe");
+        return;
+      }
+      if (
+        (datosCompletos.length !== 5 && datosCompletos.includes(undefined)) ||
+        datosCompletos.includes("")
+      ) {
+        console.log("Hay campos incompletos");
+        return;
+      }
+
+      //si todo salió bien
+      console.log("Éxito");
+
+      if (this.products.length === 0) {
+        product.id = 1;
+      } else {
+        product.id = this.products[this.products.length - 1].id + 1;
+      }
+      this.products.push(product);
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(this.products, null, "\t")
+      );
+    } catch (error) {
+      console.log(`Hay un error: ${error}`);
+    }
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(idProducto) {
+    const producto = this.products.find(
+      (producto) => producto.id === idProducto
+    );
+
+    if (!producto) {
+      console.log("Not found");
+    } else {
+      console.log("El producto encontrado es", producto);
+    }
+  }
+
+  async updateProduct(idProducto, key, newValue) {
+    let productoAmodificar = this.products.find(
+      (producto) => producto.id === idProducto
+    );
+    if (!productoAmodificar) {
+      console.log("Error. El producto no existe.");
+      return;
+    }
+
+    productoAmodificar[key] = newValue;
+
+    await fs.promises.writeFile(
+      this.path,
+      JSON.stringify(this.products, null, "\t")
+    );
+    console.log("Producto actualizado");
+  }
+
+  async deleteProduct(idProducto) {
+    const productoEncontrado = this.products.find(
+      (prod) => prod.id === idProducto
+    );
+
+    if (!productoEncontrado) {
+      return console.log("No se puede borrar. El producto no existe");
+    }
+
+    try {
+      this.products.splice(idProducto - 1, 1);
+
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(this.products, null, "\t")
+      );
+      console.log("Producto borrado");
+    } catch (error) {
+      console.log(`Hubo un error al guardar los datos: ${error}`);
+      return;
+    }
+  }
+}
+
+class Product {
+  constructor(title, description, price, thumbnail, code, stock) {
+    (this.title = title),
+      (this.description = description),
+      (this.price = price),
+      (this.thumbnail = thumbnail),
+      (this.code = code),
+      (this.stock = stock);
+  }
+}
+
+export  { ProductManager, Product }
+
+
+//-------- Pruebas --------
+
+// 1) Instancio la clase
+const prueboProducto = new ProductManager();
+
+// 2) Agrego productos y pruebo las validaciones. Si hay campos vacios o si se repite el código, no se agregan
+// prueboProducto.addProduct(
+//   new Product(
+//     "Producto prueba1",
+//     "Este es un producto prueba",
+//     2030,
+//     "SinImagen",
+//     "abc143",
+//     "44"
+//   )
+// );
+
+// prueboProducto.addProduct(
+//   new Product(
+//     "Producto prueba2",
+//     "a este le falta el stock",
+//     243,
+//     "SinImagen",
+//     "woeiefjwijw"
+//   )
+// );
+
+// prueboProducto.addProduct(
+//   new Product(
+//     "Producto prueba3",
+//     "Acá repito el código",
+//     232230,
+//     "SinImagen",
+//     "abc143",
+//     "4999"
+//   )
+// );
+
+// prueboProducto.addProduct(
+//   new Product(
+//     "Producto prueba4",
+//     "Acá tiene todo bien",
+//     12345,
+//     "SinImagen",
+//     "otrocodigo",
+//     "4999"
+//   )
+// );
+
+// prueboProducto.addProduct(
+//   new Product(
+//     "",
+//     "A este le falta el titulo",
+//     12345,
+//     "SinImagen",
+//     "32rhiof2oi",
+//     "4999"
+//   )
+// // );
+// prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 2",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "aaaa111",
+//     "344"
+//   )
+// );prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 3",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "fthoigbeo",
+//     "400"
+//   )
+// );prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 4",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "egevrw",
+//     "60"
+//   )
+// );
+// prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 5",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "fgertrty",
+//     "500"
+//   )
+// );
+// prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 6",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "pouykjnr",
+//     "9004"
+//   )
+// );
+// prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 7",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "ertgerge",
+//     "1020"
+//   )
+// );
+// prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 8",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "23423",
+//     "400"
+//   )
+// );
+// prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 9",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "hgrnbgdv",
+//     "344"
+//   )
+// );
+// prueboProducto.addProduct(
+//   new Product(
+//     "producto numero 10",
+//     "descripción",
+//     12345,
+//     "SinImagen",
+//     "gfgvfre",
+//     "5"
+//   )
+// );
+
+// 3) Busco productos por id
+// prueboProducto.getProductById(2);
+
+// // 4) Borro productos por id
+// prueboProducto.deleteProduct(2);
+
+// // 5) Actualizo productos por parámetros (id, llave, valor a modificar)
+// prueboProducto.updateProduct(2, "title", "producto2");
+
+// 6) Por último obtengo el array de productos final
+// console.log(prueboProducto.getProducts())
